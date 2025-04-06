f2b66d34c5cd7f387ba60fbc92ff751419add36c
Judie2604 <shalomjudi3@gmail.com>
2025-04-07 00:11:44 +0530

    Implemented delete operation

--- SinglyLinkedList.java
+++ SinglyLinkedList.java
@@ -31,11 +31,66 @@ public class SinglyLinkedList{
         }
         System.out.print("null");
       }
+      public void insertAtBeginning(int data){
+        Node newNode = new Node(data);
+        newNode.next = head;
+        head = newNode;
+
+      }
+      public void insertAtPosition(int data, int position){
+        Node newnode = new Node(data);
+        if(position == 1){
+        insertAtBeginning(data);
+        }
+        else{
+          Node current = head;
+          for(int i = 1; i < position -1; i++){
+            current = current.next;
+          }
+          if(current == null){
+            System.out.println("Position is out of range for the position "+position);
+            return;
+          }
+          newnode.next = current.next;
+          current.next = newnode;
+         
+        }
+      }
+      public void deletefromtheEnd(){
+        if(head == null){
+          System.out.println("List is empty");
+      }
+      else if (head.next == null){
+        head = null;
+      }
+      else{
+        Node current = head;
+        while(current.next.next != null ){
+          current = current.next;
+      }
+      current.next =null;
+
+    }
+  }
+  public void deletefromtheBeginning(){
+    if(head == null){
+      System.out.println("List is empty");
+    }
+    else{
+      head = head.next;
+  }
+}
       public static void main(String[] args){
         SinglyLinkedList list = new SinglyLinkedList();
         list.insert(10);
         list.insert(20);
         list.insert(30);
+        list.insertAtPosition(30,1);
+        list.insertAtPosition(40,2);
+        list.insertAtPosition(80,4);
+        list.insertAtPosition(100,8);
+        list.deletefromtheEnd();
+        list.deletefromtheBeginning();
         list.display();
       }
       
